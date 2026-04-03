e0d725e9a8ecf9c99ae390127eedc9aa3c887004
mpenaday <mpenaday@udesa.edu.ar>
2026-04-03 20:03:27 -0300

    acomode un poco peliculas.js

--- src/components/Peliculas/Peliculas.js
+++ src/components/Peliculas/Peliculas.js
@@ -7,7 +7,7 @@ function Peliculas() {
             <section class="row cards" id="movies">
             <article class="single-card-movie">
                 <img src="https://image.tmdb.org/t/p/w500/tzrJulItjttxzoX0t3B2My46TS7.jpg" class="card-img-top"
-                    alt="...">
+                    alt="..."/>
                 <div class="cardBody">
                     <h5 class="card-title">The Thursday Murder Club</h5>
                     <p class="card-text">A group of senior sleuths passionate about solving cold cases get plunged into
@@ -18,7 +18,7 @@ function Peliculas() {
             </article>
             <article class="single-card-movie">
                 <img src="https://image.tmdb.org/t/p/w500/9PXZIUsSDh4alB80jheWX4fhZmy.jpg" class="card-img-top"
-                    alt="...">
+                    alt="..."/>
                 <div class="cardBody">
                     <h5 class="card-title">F1</h5>
                     <p class="card-text">Racing legend Sonny Hayes is coaxed out of retirement to lead a struggling
@@ -29,7 +29,7 @@ function Peliculas() {
             </article>
             <article class="single-card-movie">
                 <img src="https://image.tmdb.org/t/p/w500/A06yXys3hrCWu8xiNoHCFLTG5SH.jpg" class="card-img-top"
-                    alt="...">
+                    alt="..."/>
                 <div class="cardBody">
                     <h5 class="card-title">I Know What You Did Last Summer</h5>
                     <p class="card-text">When five friends inadvertently cause a deadly car accident, they cover up
@@ -42,7 +42,7 @@ function Peliculas() {
             </article>
             <article class="single-card-movie">
                 <img src="https://image.tmdb.org/t/p/w500/ombsmhYUqR4qqOLOxAyr5V8hbyv.jpg" class="card-img-top"
-                    alt="...">
+                    alt="..."/>
                 <div class="cardBody">
                     <h5 class="card-title">Superman</h5>
                     <p class="card-text">Superman, a journalist in Metropolis, embarks on a journey to reconcile his
@@ -60,6 +60,6 @@ function Peliculas() {
 
     </>
 
-}
+)}
 
 export default Peliculas;
