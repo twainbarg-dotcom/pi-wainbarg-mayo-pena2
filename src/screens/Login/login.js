a68835e2ae079cf853be1b9a5e450f8bbc000c36
Yannick-sys <ymayo@udesa.edu.ar>
2026-04-09 17:48:44 -0300

    Formulario login

--- src/screens/Login/login.js
+++ src/screens/Login/login.js
@@ -1,7 +1,47 @@
 import React from "react";
+import { Component } from "react";
 
-function Login() {
-  return <h1>Login</h1>;
+class formulario extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      
+      email:"",
+      password:""
+  };
+  }
+evitarSubmit(event){
+  event.preventDefault();
+}
+controlarCambios(event){
+  this.setState({email:event.target.value});
+}
+controlarPassword(event){
+  this.setState({password:event.target.value});  
+}
+render (){
+  return(
+    <form onSubmit={(event)=>this.evitarSubmit(event)}>
+      <h2 className="alert alert-primary">Iniciar sesion</h2>
+<div class="row justify-content-center">
+    
+
+      <div className="form-group">
+      <label for="email">Email</label>
+       <input type="email" className="form-control"  placeholder = "Ingresá tu email" onChange={(event) =>this.controlarCambios(event)} value={this.state.valor}/>
+       
+      <label for="password">Password</label>
+      <input type="password" className="form-control"  placeholder = "Ingresá tu contraseña"  onChange={(event) =>this.controlarCambios(event)} value={this.state.valor}/>
+      </div>
+      <input type="submit" className="btn btn-primary btn-block" value="Iniciar sesion" />
+</div>
+
+    </form>
+  );
+ }
 }
 
-export default Login;
+
+
+
+export default formulario;
